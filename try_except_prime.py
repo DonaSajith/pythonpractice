7b1a0bc268bbcedbcfc701509d6e9ea76cefeebf
DonaSajith <118980523+DonaSajith@users.noreply.github.com>
2026-04-20 12:49:49 +0530

    try_except prime

---
+++ try_except_prime.py
@@ -0,0 +1,35 @@
+import time
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print("execution_time:", end - start)
+        return result
+    return wrapper
+
+@timer
+def prime(num):
+    if num <= 1:
+        print(num, "is not a prime number")
+    else:
+        prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                prime = False
+                break
+        if prime==True:
+            print(num, "is a prime number")
+        else:
+            print(num, "is not a prime number")
+        return prime
+
+try:
+    num = int(input("Enter a number: "))
+    prime(num)
+except ValueError:
+    print("Invalid input! Please enter a valid integer.")
+except Exception as e:
+    print("Something went wrong:", e)
+finally:
+    print("Program finished.")
