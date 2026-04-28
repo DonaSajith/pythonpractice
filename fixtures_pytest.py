313ad5ae12912203ac14bb61fd9a721b08d7ee03
DonaSajith <118980523+DonaSajith@users.noreply.github.com>
2026-04-28 09:06:45 +0530

    fixtures_pytest

---
+++ fixtures_pytest.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.fixture
+def sample_data():
+    print("\nSetting up data")
+    return [1, 2, 3, 4, 5]
+
+def test_sum(sample_data):
+    assert sum(sample_data) == 15
+
+def test_length(sample_data):
+    assert len(sample_data) == 5
